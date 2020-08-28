b255dc4f20414c6cf0d3206d35c8e4a7407f18e5
mirabellej <mirabellejones@gmail.com>
2020-08-28 16:09:40 +0200

    Add files via upload

--- 2_CapTouch_Instrument.ino
+++ 2_CapTouch_Instrument.ino
@@ -8,9 +8,6 @@ uint8_t pads[] = {3, 2, 0, 1, 6, 9, 10}; // this lists all of the touch pads
 uint8_t numberOfPads = sizeof(pads)/sizeof(uint8_t);
 
 void takeAction(uint8_t pad) {
-  Serial.print("Touched ");
-  Serial.println(pad); 
- int capData = CircuitPlayground.readCap(3);
   
   // this is a switch case statement that will do something different for each pad!
   // it's like an IF statement but better for when you have lots of inputs or want to do different things!
@@ -18,47 +15,44 @@ void takeAction(uint8_t pad) {
     
     //  Labeled Pin A4
     case 3:
-      // print the data
-      Serial.print("Capacitive Touch Reading: "); 
-      Serial.println(capData); 
+      Serial.println("Touched D3 / A4!"); 
       CircuitPlayground.playTone(5000, 500);
-      CircuitPlayground.setPixelColor(0, 255,   0,   0);
       break;
       
     //  Labeled Pin A5
     case 2:
+      Serial.println("Touched D2 / A5!"); 
       CircuitPlayground.playTone(4000, 500);
-      CircuitPlayground.setPixelColor(1, 255,   0,   0);
       break;
       
     //  Labeled Pin A6
     case 0:
+      Serial.println("Touched D0 / A6!"); 
       CircuitPlayground.playTone(1000, 500);
-      CircuitPlayground.setPixelColor(3, 255,   0,   0);
       break;
       
     //  Labeled Pin A7
     case 1:
+      Serial.println("Touched D1 / A7!"); 
       CircuitPlayground.playTone(800, 500);
-      CircuitPlayground.setPixelColor(4, 255,   0,   0);
       break;
       
     // Labeled Pin A1
     case 6:
+      Serial.println("Touched D6 / A1!"); 
       CircuitPlayground.playTone(300, 500);
-      CircuitPlayground.setPixelColor(6, 255,   0,   0);
       break;
       
     // Labeled Pin A2 
     case 9:
+      Serial.println("Touched D9 / A2!"); 
       CircuitPlayground.playTone(200, 500);
-      CircuitPlayground.setPixelColor(8, 255,   0,   0);
       break;
       
     // Labeled Pin A3 
     case 10:
+      Serial.println("Touched D10 / A3!"); 
       CircuitPlayground.playTone(100, 500);
-      CircuitPlayground.setPixelColor(9, 255,   0,   0);
       break;
       
     // It's nice to include a default case... just in case! 
@@ -69,7 +63,7 @@ void takeAction(uint8_t pad) {
 
 boolean capButton(uint8_t pad) {
   // Check if capacitive touch exceeds threshold.
-  if (CircuitPlayground.readCap(pad) > CAP_THRESHOLD) {
+  if (CircuitPlayground.readCap(pad) >= CAP_THRESHOLD) {
     return true;  
   } else {
     return false;
@@ -96,7 +90,6 @@ void loop() {
       
       // But not too often.
       delay(DEBOUNCE);
-      CircuitPlayground.clearPixels();
     }
   }
 }
