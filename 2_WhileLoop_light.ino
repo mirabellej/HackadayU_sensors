b63c48e73b94a2114c6f9aba1765e2a1debe5a86
mirabellej <mirabellejones@gmail.com>
2020-09-04 19:33:55 +0200

    Add files via upload

---
+++ 2_WhileLoop_light.ino
@@ -0,0 +1,25 @@
+#include <Adafruit_CircuitPlayground.h> // include the adafruit circuit playground library 
+
+int value; // this variable will store our light sensor value allowing us to do THINGS with it!
+int THRESHOLD = 100; // at what value or lower do we trigger our interactivity?
+
+void setup() {
+  Serial.begin(9600); // initialize serial
+  CircuitPlayground.begin(); // initialize Circuit Playground
+}
+
+void loop() {
+  value = CircuitPlayground.lightSensor(); // update "value" to store the light sensor reading
+
+  Serial.print("Light Sensor: "); // print out "light sensor"
+  Serial.println(value); // print the current value for the light sensor
+
+  while (value > THRESHOLD) {
+    CircuitPlayground.playTone(50,1000); 
+    Serial.println("TRIGGER INTERACTIVITY");
+    delay(3000);
+    value = CircuitPlayground.lightSensor(); // update "value" to store the light sensor reading
+  }
+
+  delay(100); // delay of 1 second (1000 milliseconds) between readings
+}
