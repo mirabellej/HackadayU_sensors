3e9b02251c8da992bd727e47e04e179a50b45fdc
mirabellej <mirabellejones@gmail.com>
2020-09-13 09:25:46 +0200

    Add files via upload

---
+++ 3_pseudocode.ino
@@ -0,0 +1,33 @@
+// include adafruit cp library
+// include library needed for accelerometer
+// include FastLED
+
+// define cap touch pin
+// define threshold 
+
+// anything else needed by accelerometer
+
+// anything needed by FastLED
+
+void setup() {
+  // begin serial communication
+  // initiate circuit playground express
+  // set up for our accelerometer (interrupt, etc.)
+  // FastLED setup 
+}
+
+void loop() {
+
+  // DOUBLE TAP CHECK / RESPONSE
+  // check to see if the accelerometer has been tapped
+  // if yes, play a tone
+
+  // CAP TOUCH CHECK / RESPONSE
+  // assign cap touch reading to variable
+  
+  // check to see if cap touch pin has been touched
+  // if yes, check to see if the slide switch is set to on
+        // if yes, turn all the LEDs to RED
+        // if no, turn all the LEDs to GREEN
+  // if no, display our default breathing pattern for our LEDs
+}
